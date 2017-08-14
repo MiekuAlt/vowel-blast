bf90d4fd6e309aae00de7a97b15338a58a303d44
Mieku <m.altair@live.com>
2017-08-14 19:16:25 -0300

    A random vowel is determined and displayed in the GUI

---
+++ Vowel Blast/Assets/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public char vowel;
+    private char[] possibleVowels = { 'A', 'E', 'I', 'O', 'U', 'Y' };
+
+	// Use this for initialization
+	void Start () {
+        vowel = RandomVowel();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public char GetVowel()
+    {
+        return vowel;
+    }
+
+    private char RandomVowel()
+    {
+        int index = Random.Range(0, possibleVowels.Length);
+        return possibleVowels[index];
+    }
+}
